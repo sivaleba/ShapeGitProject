2fa5dccaf5f2df792e0529a1cdff97c2c1f6cf65
sivaleba <96391621+sivaleba@users.noreply.github.com>
2022-06-15 14:30:47 -0500

    Update Shape.java

--- src/classwork/Shape.java
+++ src/classwork/Shape.java
@@ -4,6 +4,8 @@ public class Shape {
 
 	public static void main(String[] args) {
 	System.out.println("It's a class ");
+	System.out.println("This is modified by otherprogrammer.");	
+		System.out.println("It's modified in git. ");
 	}
 
 }
